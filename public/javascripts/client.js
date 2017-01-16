50d15771a1f81913525b7d3e653bafeb23ac6f2e
Andy Schott <andy@andyschott.com>
2017-01-15 22:21:45 -0500

    populate the editor when Edit is pressed

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -9,6 +9,33 @@ window.client = function() {
         // var items = document.getElementById('items');
       };
       req.send();
+    },
+
+    'startEditItem' : function(itemId) {
+      var itemNode = document.getElementById('item' + itemId);
+      var commentNode = document.getElementsByClassName('comment')[0];
+      var item = {
+        'id' : itemId,
+        'name' : itemNode.dataset.name,
+        'priority' : itemNode.dataset.priority,
+        'comment' : commentNode.innerText
+      };
+
+      var editor = document.getElementById('editor');
+      document.getElementById('editName').value = item.name;
+      document.getElementById('editPriority').value = item.priority;
+      document.getElementById('editComment').value = item.comment;
+      document.getElementById('editId').value = item.id;
+      editor.style = 'display: block;';
+    },
+
+    'editItem' : function() {
+      // var req = new XMLHttpRequest();
+      // req.open('PUT', '/avpi/v1/items/' + itemId);
+      // req.onload = function() {
+
+      // };
+      // req.send();
     }
   }
 
